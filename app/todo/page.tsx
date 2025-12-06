6bc42eea3b6c7c91b3ba7a8bfa1e09a252ba73d4
박시은 <charm516@naver.com>
2025-12-06 22:46:15 +0900

    [feat] - use-reducer 적용

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,40 +1,69 @@
 "use client";
 
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import TodoCount from "../components/TodoCount";
 import TodoEditor from "../components/TodoEditor";
 import TodoHeader from "../components/TodoHeader";
 import TodoList from "../components/TodoList";
 import TodoSearch from "../components/TodoSearch";
 import { mockData } from "../mockData";
+import { Todo } from "../types/Todo";
+
+// 1. 액션 타입 정의
+type Action =
+  | { type: "CREATE"; data: Todo }
+  | { type: "DELETE"; targetId: string }
+  | { type: "UPDATE"; targetId: string };
+
+// 2. 리듀서 함수 생성
+const reducerTodo = (state: Todo[], action: Action) => {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "DELETE":
+      return state.filter((todo) => todo.id !== action.targetId);
+    case "UPDATE":
+      return state.map((todo) =>
+        todo.id === action.targetId ? { ...todo, isDone: !todo.isDone } : todo,
+      );
+    default:
+      return state;
+  }
+};
 
 const TodoPage = () => {
-  const [todos, setTodos] = useState(mockData);
+  // 3. useReducer 훅 호출
+  const [state, dispatch] = useReducer(reducerTodo, mockData);
   const [search, setSearch] = useState("");
 
+  // 4. 액션 객체 전달
   const addTodo = (content: string) => {
-    const newTodo = {
-      id: crypto.randomUUID(),
-      content: content,
-      isDone: false,
-      time: new Date().getTime(),
-    };
-    setTodos([newTodo, ...todos]);
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: crypto.randomUUID(),
+        content: content,
+        isDone: false,
+        time: new Date().getTime(),
+      },
+    });
   };
 
   const toggleTodo = (targetId: string) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo,
-      ),
-    );
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
   };
 
   const deleteTodo = (targarId: string) => {
-    setTodos(todos.filter((todo) => todo.id !== targarId));
+    dispatch({
+      type: "DELETE",
+      targetId: targarId,
+    });
   };
 
-  const filteredTodos = todos.filter((todo) =>
+  const filteredTodos = state.filter((todo) =>
     todo.content.toLowerCase().trim().includes(search.toLowerCase().trim()),
   );
 
